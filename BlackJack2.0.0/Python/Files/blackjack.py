e5c9a7616c4fbe47a8707d0616fe2c5b17f818ff
Dezeli <haterecursive@gmail.com>
2024-01-03 18:54:19 +0900

    Label & Button Maker

--- BlackJack2.0.0/Python/Files/blackjack.py
+++ BlackJack2.0.0/Python/Files/blackjack.py
@@ -1,19 +1,29 @@
+import os
+
+from maker import maker
 from tkinter import *
-
-
-
+from typing import *
 
 
 class Game_Screen:
     def __init__(self) -> None:
+        """
+        ### Black Jack 게임 실행 Class
+        """
         self.screen = Tk()
         self.screen.title("Black Jack 2.0.0")
         self.set_screen_size()
+        self.img_maker = maker(self.screen)
+        self.python_path = os.path.join(os.getcwd())
+        self.menu_screen()
         self.screen.mainloop()
 
     def set_screen_size(self) -> None:
-        screen_width = 1200
-        screen_height = 700
+        """
+        ### 화면 크기 및 위치 설정 함수
+        """
+        screen_width = 1204
+        screen_height = 704
         windows_width = self.screen.winfo_screenwidth()
         windows_height = self.screen.winfo_screenheight()
         center_width = (windows_width - screen_width)//2
@@ -21,8 +31,20 @@ class Game_Screen:
         self.screen.geometry(f"{screen_width}x{screen_height}-{center_width}-{center_height}")
         self.screen.resizable(width=False, height=False)
 
+    def img_path(self, path:str) -> str:
+        """
+        ### 이미지 경로 지정 함수
+        path : 이미지 파일 경로 (Images 파일 하위 경로만)
+        """
+        return os.path.join(self.python_path, f"../../images/{path}")
+
+    def menu_screen(self) -> None:
+        """
+        ### 메뉴 화면 구현 함수
+        """
+        menu_bg = self.img_maker.make_img(self.img_path("Bg/menu.png"), [0, 0])
+        
 
 
-
-if __name__ == "__main__":
-    run = Game_Screen()
+if __name__=="__main__":
+    execute = Game_Screen()
---
+++ BlackJack2.0.0/Python/Files/maker.py
@@ -0,0 +1,90 @@
+import os
+from tkinter import *
+from typing import *
+from PIL import ImageTk
+
+class maker:
+    def __init__(self, screen:Tk):
+        """
+        ### Label과 Button을 쉽게 생성할 수 있도록 만들어주는 Class
+        screen : Label과 Button을 생성할 Tkinter Screen
+        """
+        self.screen = screen
+        self.dir_path = os.path.join(os.getcwd(), "images")
+
+    def make_label(self, pos:List[int], txt:List) -> Label:
+        """
+        ### Label을 생성하는 함수
+        pos : [x좌표, y좌표]
+        txt : [내용, 글자 색, 배경 색, (폰트, 폰트 크기)]
+        """
+        label = Label(
+            self.screen,
+            text = txt[0],
+            fg = txt[1],
+            bg = txt[2],
+            font = txt[3],
+            height=1,
+        )
+        label.place(x=pos[0], y=pos[1])
+        return label
+
+    def make_img(self, file_name:str, pos:List[int]) -> Label:
+        """
+        ### Image Label을 생성하는 함수
+        file_name : 사진 파일 경로
+        pos : [x좌표, y좌표]
+        """
+        final_path = os.path.join(self.dir_path, file_name)
+        img = ImageTk.PhotoImage(file=final_path)
+        img_label = Label(self.screen)
+        img_label.configure(image=img)
+        img_label.image = img
+        img_label.place(x=pos[0], y=pos[1])
+        return img_label
+
+    def make_txt_img(self, file_name:str, pos:List[int], txt:List) -> Label:
+        """
+        ### Image Text Label을 생성하는 함수
+        file_name : 사진 파일 경로
+        pos : [x좌표, y좌표]
+        txt : [내용, 글자 색, (폰트, 폰트 크기)]
+        """
+        final_path = os.path.join(self.dir_path, file_name)
+        img = ImageTk.PhotoImage(file=final_path)
+        img_label = Label(self.screen, text=txt[0], fg=txt[1], font=txt[2], compound=CENTER)
+        img_label.configure(image=img)
+        img_label.image = img
+        img_label.place(x=pos[0], y=pos[1])
+        return img_label
+
+    def make_btn_img(self, file_name:str, pos:List[int], cmd:function) -> Button:
+        """
+        ### Image Button을 생성하는 함수
+        file_name : 사진 파일 경로
+        pos : [x좌표, y좌표]
+        cmd : 이벤트 함수
+        """
+        final_path = os.path.join(self.dir_path, file_name)
+        img = ImageTk.PhotoImage(file=final_path)
+        img_btn = Button(self.screen, command=cmd, overrelief=SOLID)
+        img_btn.configure(image=img)
+        img_btn.image = img
+        img_btn.place(x=pos[0], y=pos[1])
+        return img_btn
+
+    def make_txt_btn_img(self, file_name:str, pos:List[int], cmd:function, txt:List) -> Button:
+        """
+        ### Image Text Label을 생성하는 함수
+        file_name : 사진 파일 경로
+        pos : [x좌표, y좌표]
+        cmd : 이벤트 함수
+        txt : [내용, 글자 색, (폰트, 폰트 크기)]
+        """
+        final_path = os.path.join(self.dir_path, file_name)
+        img = ImageTk.PhotoImage(file=final_path)
+        img_btn = Button(self.screen, command=cmd, overrelief=SOLID, text=txt[0], fg=txt[1], font=txt[2], compound=CENTER, justify=LEFT)
+        img_btn.configure(image=img)
+        img_btn.image = img
+        img_btn.place(x=pos[0], y=pos[1])
+        return img_btn
